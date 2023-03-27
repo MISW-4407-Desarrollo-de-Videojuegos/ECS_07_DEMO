ee4020e20565a07d78426702a49170d4916be7bd
Italo Capasso <edwood.grant@gmail.com>
2023-03-27 15:12:11 -0500

    oreccines de algunos casos borde.

--- src/ecs/systems/s_ball_steering.py
+++ src/ecs/systems/s_ball_steering.py
@@ -2,8 +2,9 @@ import pygame
 import esper
 from src.ecs.components.c_steering import CSteering
 
-from src.ecs.components.c_velocity import CVelocity
+from src.ecs.components.c_surface import CSurface
 from src.ecs.components.c_transform import CTransform
+from src.ecs.components.c_velocity import CVelocity
 from src.ecs.components.tags.c_tag_ball import CTagBall
 from src.ecs.components.tags.c_tag_block import CTagBlock
 
@@ -14,9 +15,9 @@ def system_ball_steering(world:esper.World, ball_cfg:dict, delta_time:float):
         closest_bad, bad_block_dist = _get_closest_bad_block_pos(world, c_t.pos)
 
         # Follow Vector
-        follow_dampening = good_block_dist / 100
+        follow_dampening = good_block_dist / 40
         c_bs.follow_vector = closest_good - c_t.pos
-        desired_good_length = min(ball_cfg["follow_force"], ball_cfg["follow_force"] / follow_dampening)
+        desired_good_length = ball_cfg["follow_force"]
         c_bs.follow_vector.scale_to_length(desired_good_length)
 
         # Avoid Vector
@@ -31,24 +32,22 @@ def system_ball_steering(world:esper.World, ball_cfg:dict, delta_time:float):
 def _get_closest_good_block_pos(world:esper.World, ball_pos:pygame.Vector2) -> pygame.Vector2:
     min_pos:pygame.Vector2 = None
     min_dist = None
-    components = world.get_components(CTransform, CTagBlock)
-    for _, (c_t, c_bl) in components:
+    components = world.get_components(CTransform, CSurface, CTagBlock)
+    for _, (c_t, c_s, c_bl) in components:
         if c_bl.b_type == "B1":
             dist = c_t.pos.distance_to(ball_pos)
             if min_pos is None or dist < min_dist:
-                min_pos = c_t.pos
+                min_pos = c_t.pos + c_s.area.center
                 min_dist = dist
-    
     return min_pos, min_dist
 
 def _get_closest_bad_block_pos(world:esper.World, ball_pos:pygame.Vector2) -> pygame.Vector2:
     min_pos:pygame.Vector2 = None
-    components = world.get_components(CTransform, CTagBlock)
-    for _, (c_t, c_bl) in components:
+    components = world.get_components(CTransform, CSurface, CTagBlock)
+    for _, (c_t, c_s, c_bl) in components:
         if c_bl.b_type == "B5":
             dist = c_t.pos.distance_to(ball_pos)
             if min_pos is None or dist < min_dist:
-                min_pos = c_t.pos
+                min_pos = c_t.pos + c_s.area.center
                 min_dist = dist
-    
     return min_pos, min_dist
